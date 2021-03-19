5e52077c7c9b24f1ee5872fc9244b88d4ba64808
raveena69 <58651579+raveena69@users.noreply.github.com>
2021-03-20 00:16:19 +0530

    Update the objectandPython.js file

--- objectAndPython.js
+++ objectAndPython.js
@@ -6,6 +6,9 @@ let student = {
     last: 'Qazi',
     age:25,
     height:171,
+    studentInfo: function(){
+        return this.first + '\n' + this.last;
+    }
 };
 
 console.log(student.first); //output --> Ryan
@@ -13,8 +16,13 @@ console.log(student.last);  //output --> Qazi
 console.log(student['first']); //output --> Ryan
 console.log(student['age']); //output --> 25
 
-student['first'] = 'notRafesh'; // change value
-console.log(student.first);  // output --> notRafesh
+// student['first'] = 'notRafesh'; // change value
+// console.log(student.first);  // output --> notRafesh
 
 student.age++;
 console.log(student.age);
+
+console.log(student.studentInfo()); // output --> Ryan
+                                    //            Qazi
+//because of this --> student['first'] = 'notRafesh';  and console.log(student.first);  
+// console.log(student.studentInfo());  // you should only run when the previous line comment, if not this line doesn't give the output
